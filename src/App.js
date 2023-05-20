fb3e0c7068325630fd57418c28cc12baef9784e1
jerremy <jerrremyng@gmail.com>
2023-05-20 15:00:28 +0800

    login page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 import { useState } from 'react';
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtCKR5oHaQcmb4ff8LVXZDt8XP2vl6RLc",
@@ -19,41 +20,98 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 
 export default function App() {
+
+  const isLogin = !useAuthState(auth);
+
   return (
     <div className="App">
       <nav>
         <img src={require("./assets/logo.png")}/>
-        <Login />
       </nav>
 
+      {isLogin ? <br/> : <Login/>}
     </div>
   );
 }
 
 function Login() {
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
+
+  //login things
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      document.write(errorCode + errorMessage)
+    })
+  }
 
   const googleLogin = () => {
     const provider = new GoogleAuthProvider();
     signInWithRedirect(auth,provider);
-    document.write("yes")
+    //document.write("yes")
   }
+
+  //main code
   return (
-    <>
-    <h2>jerremy is dum dum!</h2>    
-    <form>
+    <> 
+    <h2>Welcome</h2>    
+    <h4>please sign in</h4>
+    <form onSubmit={handleSubmit}>
       <label for="email">email</label>
       <input value={email} type='email' placeholder='youremail@email.com'></input>
       <label for='password'>password</label>
       <input value={password} type='password' placeholder='password'></input>
-      <button>Login</button>
+      <button type='submit'>Login</button>
+
     </form>
+    <button>Dont have an account?</button>
 
     <br></br>
     <div>or</div>
     <br></br>
     <button onClick={googleLogin}>Google Login</button>
+    </>
+
+  )
+}
+
+function Register() {
+  
+  //login things
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password2, setPassword2] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    createUserWithEmailAndPassword(auth, email, password)
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      document.write(errorCode + errorMessage)
+    })
+  }
+
+  //main code
+  return (
+    <> 
+    <h2>Create Account</h2>    
+    <form onSubmit={handleSubmit}>
+      <label for="email">email</label>
+      <input value={email} type='email' placeholder='youremail@email.com'></input>
+      <label for='password'>password</label>
+      <input value={password} type='password' placeholder='password'></input>
+      <label for='password2'>Re-enter password</label>
+      <input value={password2} type='password' placeholder='re-enter password'></input>
+      <button type='submit'>Login</button>
+
+    </form>
+    <button>Already have an account?</button>
     </>
 
   )
