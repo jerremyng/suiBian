512cd9e0f2f7c3d684a8b923ae32f06eb116e077
jerremy <jerrremyng@gmail.com>
2023-05-20 13:59:21 +0800

    some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtCKR5oHaQcmb4ff8LVXZDt8XP2vl6RLc",
+  authDomain: "suibian-6654f.firebaseapp.com",
+  databaseURL: "https://suibian-6654f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "suibian-6654f",
+  storageBucket: "suibian-6654f.appspot.com",
+  messagingSenderId: "272835399367",
+  appId: "1:272835399367:web:bd4dd1d1a10a4b9bfbf8e2",
+  measurementId: "G-95Z540EN96"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <img src={require("./assets/logo.png")}/>
+        <Login />
+      </nav>
+
     </div>
   );
 }
 
-export default App;
+function Login() {
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+  const googleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth,provider);
+    document.write("yes")
+  }
+  return (
+    <>
+    <h2>jerremy is dum dum!</h2>    
+    <form>
+      <label for="email">email</label>
+      <input value={email} type='email' placeholder='youremail@email.com'></input>
+      <label for='password'>password</label>
+      <input value={password} type='password' placeholder='password'></input>
+      <button>Login</button>
+    </form>
+
+    <br></br>
+    <div>or</div>
+    <br></br>
+    <button onClick={googleLogin}>Google Login</button>
+    </>
+
+  )
+}
