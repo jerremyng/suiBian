5a2efe268fc2a021f7a79f11d7b0f112a5886844
jerremy <jerrremyng@gmail.com>
2023-06-20 00:23:55 +0800

    some more change

--- src/components/FoodLocation.js
+++ src/components/FoodLocation.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-
-function FoodLocation({ image, name, location }) {
-    return (
-      <div className="foodLocation">
-        <div style={{ backgroundImage: `url(${image})` }}> </div>
-        <h3> {name} </h3>
-        <h3 className="med"> {location} </h3>
-        <Link to="/about">
-          <button>Find out more</button>
-        </Link>
-      </div>
-    );
-  }
-  
-  export default FoodLocation;
-
-
-  // {trialdata.map((foodLocation, key) => {
-  //   return (
-  //       <FoodLocation
-  //           key={key}
-  //           image={foodLocation.image}
-  //           name={foodLocation.name}
-  //           location={foodLocation.location}
+export default function FoodLocation({image, name, location}) {
+  return (
+    <div className="foodLocation">
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h3> {name} </h3>
+      <h3 className="med"> {location} </h3>
+      <Link to="/about">
+        <button>View place</button>
+      </Link>
+    </div>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -15,43 +15,33 @@ export default function Home() {
     var user = fireBaseAuth.currentUser;
     const [results, setResults] = useState([]);
     return (
-
-        <><React.Fragment>
+        <>
             <Navbar />
-        </React.Fragment>
             <section>
                 <div className='tophalf'>
                     <div className='fake-header'>
-                        <div className='div1'>
-                            <img src={require("../assets/logo2.png")}
-                            style={{ width: 140, height: 140 }} />
-                        </div>
+
                         <div className='div2'>
                             {user == null 
                             ? <h2>Let Suibian be your guide</h2> 
                             : <h2>Welcome Back {user.email}</h2>}
-                            {/* <h2>Let SuiBian be your guide</h2> */}
                         </div>
                     </div>
 
                     <div className='mid'> 
                         <div className='div3'>
                             <h2 className='dont'>
-                                don't know what to eat?
+                                Whats for Lunch?
                             </h2>
-                            <button className='lucky'>decide now!</button>
+                            <button className='lucky'>Feeling lucky?</button>
                         </div>
                         <div className='div4'>
                             <img src={require("../assets/smol.png")}
                                 style={{ height: 300 }}/>
                         </div>
                     </div>
-                    <div className='div5'>
-                        <Searchbar/>
-                    </div>
-                    <div class="div3">
-                        <img src={require("../assets/smol.png")}/>
-                    </div> */}
+
+                    <Searchbar/>
                     
                     {/* <div className='div2'>
                         <Searchbar setResults={setResults}/>
@@ -73,14 +63,12 @@ export default function Home() {
                             );
                         })}
                         <br></br>
-                    </div> 
-                {/* <Search></Search> */}
+                    </div> */}
                     
-                </div> */}
 
             </section>
             
-            </>
+        </>
     );
 }
 
