86556a1a7988078aec04684be7f9705f469ca068
kmkmkmkmkmkmmm <theglxssylala@gmail.com>
2023-06-21 17:39:53 +0800

    poopy

--- src/components/card.js
+++ src/components/card.js
@@ -5,7 +5,7 @@ import Modal from './Modal'
 export default function Card({foo}) {
   const [modalOpen, setModalOpen] = useState(false);
   return (
-      <div className="foodLocation">
+      <div  className="foodLocation">
         <h3> {foo["name"]} </h3>
         <h3 className="med"> {foo["address"]["buildingName"]} </h3>
           <button
@@ -13,7 +13,7 @@ export default function Card({foo}) {
             onClick={() => {
               setModalOpen(true);
             }}
-          >Find out more</button>
+          >Find out more!</button>
         {modalOpen && <Modal setOpenModal={setModalOpen} foo = {foo}/>}  
       </div>
   );
