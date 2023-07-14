fe26ba07289c85ea365c697b23391a2fa1632c83
kmkmkmkmkmkmmm <theglxssylala@gmail.com>
2023-07-14 16:32:10 +0800

    pls werkkk

---
+++ src/components/PlaceFinder.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default class PlaceFinder {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  async getNearbyPlaces(query, lat, long, limit = 5, radius = 10000) {
+    let baseUrl = 'https://api.tomtom.com/search/2/poiSearch';
+    let queryString = `limit=${limit}&lat=${lat}&lon=${long}&radius=${radius}&key=${this.apiKey}`;
+    let response = await axios.get(`${baseUrl}/${query}.json?${queryString}`);
+    return response.data.results;
+  }
+}
---
+++ src/components/Searchbar2.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import PlaceFinder from './PlaceFinder';
+import Place from './Place';
+import Banner from '../components/Banner';
+import ReactSearchBox from "react-search-box";
+
+class Searchbar2 extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            geoLocation : {},
+            geoError: null,
+            searchResults: [],
+        };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition((e) => {
+            this.setState({
+                geoLocation: e.coords
+            });
+        }, async(err) => {
+            this.setState({
+                geoError: err
+            });
+        });
+    }
+
+    async onSearchChange(query) {
+        if (query.length > 0) {
+          let placeFinder = new PlaceFinder('bXTqlI8FRoRHygyBJmLIQ3rm1YzCy6u3');
+          let results = await placeFinder.getNearbyPlaces(
+            query,
+            this.state.geoLocation.latitude,
+            this.state.geoLocation.longitude
+          );
+          this.setState({
+            searchResults: results
+          });
+        }
+      }
+
+    setPlace(key) {
+        let place = this.state.searchResults.find(p => p.id === key);
+            this.setState({
+            selectedPlace: place
+        });
+    }
+      
+      
+    render() {
+        return (
+          <div>
+            <Banner
+              geoLocation={this.state.geoLocation}
+              geoError={this.state.geoError}
+            />
+
+
+            <ReactSearchBox
+              placeholder="Search for nearby places"
+              matchedRecords={this.state.searchResults.map(result => ({
+                key: result.id,
+                name: result.poi.name,
+                dist: result.dist,
+                value: `${result.poi.name} | ${(result.dist / 1000).toFixed(2)}km`
+              })).sort((a, b) => a.dist - b.dist)}
+              data={this.state.searchResults.map(result => ({
+                key: result.id,
+                name: result.poi.name,
+                dist: result.dist,
+                value: result.poi.name
+              })).sort((a, b) => a.dist - b.dist)}
+              onSelect={place => this.setPlace(place.key)}
+              autoFocus={true}
+              onChange={query => this.onSearchChange(query)}
+              fuseConfigs={{
+                minMatchCharLength: 0,
+                threshold: 1,
+                distance: 100000,
+                sort: false
+              }}
+              keys={['name']}
+            />
+
+            <Place 
+                className="place-box" 
+                data={this.state.selectedPlace}>
+            </Place>
+
+          </div>
+        );
+    }
+      
+}
+
+export default Searchbar2;
